d22da25c4919b46c962e14f21db9106ce4172a03
Ruben Knaus <ruben.knaus@gmail.com>
2011-12-18 20:08:27 +0100

    added list polls and user settings button

--- src/ch/netgeek/easypollclient/EasyPollClientActivity.java
+++ src/ch/netgeek/easypollclient/EasyPollClientActivity.java
@@ -2,6 +2,9 @@ package ch.netgeek.easypollclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class EasyPollClientActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,29 @@ public class EasyPollClientActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button listPollsButton = (Button) findViewById(R.id.button_list_polls);
+        listPollsButton.setOnClickListener(
+                new View.OnClickListener() {
+                    
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(EasyPollClientActivity.this, 
+                                "List Polls...", Toast.LENGTH_LONG).show();
+                        
+                    }
+                });
+        
+        Button userSettingsButton = (Button) findViewById(R.id.button_user_settings);
+        userSettingsButton.setOnClickListener(
+                new View.OnClickListener() {
+                    
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(EasyPollClientActivity.this, 
+                                "User Settings...", Toast.LENGTH_LONG).show();
+                        
+                    }
+                });
     }
 }
